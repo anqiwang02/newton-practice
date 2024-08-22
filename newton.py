3e6dd78579d87032a61891c3d15a1079def16d26
Your Name <your_name@berkeley.edu>
2024-08-22 13:18:39 -0700

    lint

--- newton.py
+++ newton.py
@@ -1,59 +1,39 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.2
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
+x0 = 1
 
-# +
-from sympy import diff, ln
-from sympy.abc import x
-import math
 
-f = 2*x**2 #function 
-df = diff(f) #first derivative 
-ddf = diff(df) #second derivative 
-print(df)
-print(ddf)
-
-x0=1
-
-#a function of choice
+# a function of choice
 def f(x):
-    y=2*x**2
+    y = 2 * x**2
     return y
 
+
 def d_f(x):
-    h=10**(-5) #make it infintely small
-    return (f(x+h)-f(x))/h #first derivative of fun(x)
+    h = 10 ** (-5)  # make it infintely small
+    return (f(x + h) - f(x)) / h  # first derivative of fun(x)
+
 
 def dd_f(x):
-    h=10**(-5)
-    return (d_f(x+h)-d_f(x))/h #second derivative of fun(x)
+    h = 10 ** (-5)
+    return (d_f(x + h) - d_f(x)) / h  # second derivative of fun(x)
+
 
 for i in range(10):
-    xt=x0-d_f(x0)/dd_f(x0)
-    #update the new xt to be x0
+    xt = x0 - d_f(x0) / dd_f(x0)
+    # update the new xt to be x0
     x0 = xt
-    if abs(xt-x0)==0:
+    if abs(xt - x0) == 0:
         break
 
 xt
 
-# +
-#import math
 
-#x0=1
+# ignore below
+# from sympy import diff, ln
+# from sympy.abc import x
+# import math
 
-#f=x**2
-#df=diff(f)
-#ddf=diff(df)
-#print(df)
-#print(ddf)
+# f = 2*x**2 #function
+# df = diff(f) #first derivative
+# ddf = diff(df) #second derivative
+# print(df)
+# print(ddf)
