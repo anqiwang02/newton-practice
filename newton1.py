aac97db12fb69a2769311360e5587be8fe1a5e79
Your Name <your_name@berkeley.edu>
2024-08-22 11:32:43 -0700

    Add basic implementation of Newton's method.

---
+++ newton1.py
@@ -0,0 +1,33 @@
+from sympy import diff, ln
+from sympy.abc import x
+import math
+
+f = 2*x**2 #function 
+df = diff(f) #first derivative 
+ddf = diff(df) #second derivative 
+print(df)
+print(ddf)
+
+x0=1
+
+#a function of choice
+def f(x):
+    y=2*x**2
+    return y
+
+def d_f(x):
+    h=10**(-5) #make it infintely small
+    return (f(x+h)-f(x))/h #first derivative of fun(x)
+
+def dd_f(x):
+    h=10**(-5)
+    return (d_f(x+h)-d_f(x))/h #second derivative of fun(x)
+
+for i in range(10):
+    xt=x0-d_f(x0)/dd_f(x0)
+    #update the new xt to be x0
+    x0 = xt
+    if abs(xt-x0)==0:
+        break
+
+xt
