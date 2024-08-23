ba1765f3e644de47d8517268853efde72752adfc
Your Name <your_name@berkeley.edu>
2024-08-23 11:17:28 -0700

    updates

--- newton.py
+++ newton.py
@@ -3,7 +3,7 @@ x0 = 1
 
 # a function of choice
 def f(x):
-    y = 2 * x**2
+    y = (x**4)/4-x**3-x
     return y
 
 def d_f(x):
@@ -14,7 +14,7 @@ def dd_f(x):
    h = 10 ** (-5)
    return (d_f(x + h) - d_f(x)) / h  # second derivative of fun(x)
 
-def optimize(x):
+def optimize(x0):
     h = 10 ** (-5)  # make it infintely small
     dev_1 = d_f(x0) # first derivative of fun(x)
     dev_2 = dd_f(x0)
@@ -23,7 +23,7 @@ def optimize(x):
     while abs(x1-x0)>=0.01:
         x0=x1
         x1=x0-dev_1/dev_2
-        return [x1,dev_1, dev_2]
+        return [x1,dev_1,dev_2]
 
 
 
